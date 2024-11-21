8c524caffc80a84d40ff462ba2689029b02b4ad0
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2024-11-21 21:43:59 +0200

    Initial packages

---
+++ Editor/UIReferencePropertyDrawer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Joi.UIReferences.Editor
+{
+	[CustomPropertyDrawer(typeof(UIReferenceFloat))]
+	[CustomPropertyDrawer(typeof(UIReferenceBool))]
+	[CustomPropertyDrawer(typeof(UIReferenceInt))]
+	[CustomPropertyDrawer(typeof(UIReferenceString))]
+	[CustomPropertyDrawer(typeof(UIReferenceGameObject))]
+	[CustomPropertyDrawer(typeof(UIReferenceVector3))]
+	public class UIReferencePropertyDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			var indent = EditorGUI.indentLevel;
+			EditorGUI.indentLevel = 0;
+
+			var typeRect = new Rect(position.x, position.y, 20, position.height);
+			var valueRect = new Rect(position.x + 20, position.y, position.width - 20, position.height);
+
+			var useVariableProperty = property.FindPropertyRelative("UseVariable");
+
+			var btnTexture = useVariableProperty.boolValue
+				? EditorGUIUtility.IconContent("preAudioPlayOn").image
+				: EditorGUIUtility.IconContent("preAudioPlayOff").image;
+
+			var valueProperty = useVariableProperty.boolValue
+				? property.FindPropertyRelative("Variable")
+				: property.FindPropertyRelative("Constant");
+
+			EditorGUI.PropertyField(valueRect, valueProperty, GUIContent.none);
+
+			EditorGUI.BeginChangeCheck();
+			if (GUI.Button(typeRect, btnTexture, GUIStyle.none))
+			{
+				useVariableProperty.boolValue = !useVariableProperty.boolValue;
+			}
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				property.serializedObject.ApplyModifiedProperties();
+			}
+
+			EditorGUI.indentLevel = indent;
+
+			EditorGUI.EndProperty();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Joi UI References
+
+Unity UI references for UI variables
+
+## Installation
+
+```json
+{
+    "dependencies": {
+        "com.joi.uivariables": "https://github.com/joakim-isaksson/joi.git#joi-uivariables",
+        "com.joi.uireferences": "https://github.com/joakim-isaksson/joi.git#joi-uireferences"
+    }
+}
+```
